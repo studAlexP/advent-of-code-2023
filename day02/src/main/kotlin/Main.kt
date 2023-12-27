cbf7e69d49e36b75bfaa24ba66433203f1fd4044
Alexander Pruka <alexander.pruka@stud.fh-campuswien.ac.at>
2023-12-27 12:16:41 +0100

    Refactoring to align with ktlint

--- day02/src/main/kotlin/Main.kt
+++ day02/src/main/kotlin/Main.kt
@@ -20,8 +20,6 @@ fun processGames(games: List<String>): Pair<Int, Int> {
         val rounds = game.split(";").filter { it.isNotBlank() }
         val (roundPossible, gameCounts) = processRounds(rounds)
 
-
-
         if (roundPossible) {
             sumOfPossibleGames += index + 1 // Adding the index (game number) of the possible game
         }
@@ -60,15 +58,16 @@ fun readGames(fileWithGames: File): List<String> {
 }
 
 fun extractCubeCounts(roundInput: String): CubeCounts {
-    val counts = roundInput.split(",")
-        .fold(CubeCounts(0, 0, 0)) { acc, cube ->
-            when {
-                cube.contains("red") -> acc.copy(red = acc.red + extractNumber(cube))
-                cube.contains("green") -> acc.copy(green = acc.green + extractNumber(cube))
-                cube.contains("blue") -> acc.copy(blue = acc.blue + extractNumber(cube))
-                else -> acc
+    val counts =
+        roundInput.split(",")
+            .fold(CubeCounts(0, 0, 0)) { acc, cube ->
+                when {
+                    cube.contains("red") -> acc.copy(red = acc.red + extractNumber(cube))
+                    cube.contains("green") -> acc.copy(green = acc.green + extractNumber(cube))
+                    cube.contains("blue") -> acc.copy(blue = acc.blue + extractNumber(cube))
+                    else -> acc
+                }
             }
-        }
 
     return counts
 }
@@ -83,6 +82,6 @@ fun isGamePossible(cubeCounts: CubeCounts): Boolean {
     val MAX_NUMBER_OF_BLUE_CUBES = 14
 
     return cubeCounts.red <= MAX_NUMBER_OF_RED_CUBES &&
-            cubeCounts.green <= MAX_NUMBER_OF_GREEN_CUBES &&
-            cubeCounts.blue <= MAX_NUMBER_OF_BLUE_CUBES
+        cubeCounts.green <= MAX_NUMBER_OF_GREEN_CUBES &&
+        cubeCounts.blue <= MAX_NUMBER_OF_BLUE_CUBES
 }
